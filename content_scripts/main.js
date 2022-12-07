17202c3b572e8d1e3e4ad2e74fbec1683f306929
rems-38 <rem.mazzone@orange.fr>
2022-12-07 10:34:08 +0100

    start popup

---
+++ content_scripts/main.js
@@ -0,0 +1,4 @@
+
+let header2 = document.createElement('h3');
+header2.textContent = "N'oubliez pas d'ajouter la commande dans la base de données !";
+document.querySelector("[role='banner']").appendChild(header2);
