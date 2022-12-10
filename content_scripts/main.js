3541b3ed7fd689f3912bdd1c7892cd7659f6ed4c
rems-38 <rem.mazzone@orange.fr>
2022-12-10 17:08:37 +0100

    front end form

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,4 +1,3 @@
-
-let header2 = document.createElement('h3');
-header2.textContent = "N'oubliez pas d'ajouter la commande dans la base de données !";
-document.querySelector("[role='banner']").appendChild(header2);
+// let header2 = document.createElement('h3');
+// header2.textContent = "N'oubliez pas d'ajouter la commande dans la base de données !";
+// document.querySelector("[role='banner']").appendChild(header2);
